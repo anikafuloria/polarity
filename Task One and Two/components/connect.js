ffd67285e3909d4a2e20617f9dbe391d6e43e88e
Anika Fuloria <anikafuloria@DN51r81t.SUNet>
2022-12-08 14:12:45 -0800

    Updated the connect.js page to have correct header/footer.

--- Task One and Two/components/connect.js
+++ Task One and Two/components/connect.js
@@ -6,19 +6,25 @@ import { Entypo } from '@expo/vector-icons';
 export default function Body ({ navigation }) {
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => navigation.navigate('OneOnOnes')}>
-        <View style={styles.speaker}>
-          <Text style={{fontSize: 20}}>1-on-1</Text>
-          <Text style={{fontSize: 20}}>Meet others with bipolar disorder</Text>
-        </View>
-      </Pressable>
 
-      <Pressable onPress={() => navigation.navigate('Speaker')}>
-        <View style={styles.speaker}>
-          <Text style={{fontSize: 20}}>Speaker</Text>
-          <Text style={{fontSize: 20}}>Share your experience with others.</Text>
+      <View style={styles.body}>
+        <View style={styles.panda}>
+          <Image source={require("../assets/PandaHeader.png")}/>
         </View>
-      </Pressable>
+        <Pressable onPress={() => navigation.navigate('OneOnOnes')}>
+          <View style={styles.speaker}>
+            <Text style={{fontSize: 20}}>1-on-1</Text>
+            <Text style={{fontSize: 20}}>Meet others with bipolar disorder</Text>
+          </View>
+        </Pressable>
+
+        <Pressable onPress={() => navigation.navigate('Speaker')}>
+          <View style={styles.speaker}>
+            <Text style={{fontSize: 20}}>Speaker</Text>
+            <Text style={{fontSize: 20}}>Share your experience with others.</Text>
+          </View>
+        </Pressable>
+      </View>
       <Footer navigation={navigation}/>
     </View>
   );
@@ -27,26 +33,37 @@ export default function Body ({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     backgroundColor: '#CFDDA8',
-    paddingBottom: 300,
   },
   ononone: {
     backgroundColor: '#FFFFFF',
     borderRadius: 10,
     height: 125,
-    width: 130,
+    width: 250,
     alignItems: 'center',
     paddingTop: 20,
+  },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#CFDDA8',
+    paddingBottom: 300,
   },
   speaker: {
     backgroundColor: '#FFFFFF',
     borderRadius: 10,
     height: 125,
-    width: 130,
+    width: 250,
     marginTop: 48,
     alignItems: 'center',
     paddingTop: 20,
   },
-
+  panda: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 60,
+    backgroundColor: '#CFDDA8',
+    padding: 8,
+    paddingBottom: 100,
+  },
 });
